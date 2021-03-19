a6c00e1f503c88c62bef32f6bfa4dcdc52c046f5
江峰 <feng.jiang@marketin.cn>
2021-03-19 17:03:29 +0800

    线程池配置

--- src/main/java/com/jf/template/aspect/log/LogTypeEnum.java
+++ src/main/java/com/jf/template/aspect/log/LogTypeEnum.java
@@ -7,9 +7,9 @@ package com.jf.template.aspect.log;
  * @date 2020/8/1 14:34
  */
 public enum LogTypeEnum {
-    REQUEST_PARAM,
-    RESPONSE,
-    FULL;
-    private LogTypeEnum() {
-    }
+
+	REQUEST_PARAM, RESPONSE, FULL;
+
+	LogTypeEnum() {
+	}
 }
